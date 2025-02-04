5a85f0b671045c8036f58fad7ca0abedfb4b1b9c
pianoleo1 <leozhang0331@gmail.com>
2025-02-04 09:35:16 -0800

    Create NumberGuessing.py

---
+++ NumberGuessing.py
@@ -0,0 +1,14 @@
+# Number Guessing Game
+userinput = int(input("Welcome to the number guessing game! Would you like to play computer guessing mode (1) or human mode (2)?"))
+if userinput == 1:
+  print("Entering computer guessing mode...")
+  guessnum = input("What is the maximum number that I can guess? ")
+  while True:
+    guessnum = guessnum//2
+    userinput = input("Is it",guessnum,"? (too high/too low)")
+    if userinput == "too high":
+      guessnum = guessnum//2
+      print("Is it",guessnum,"? (too high/too low)")
+    If userinput == "too low":
+      guessnum = guessnum + guessnum//2
+      print("Is it",guessnum,"? (too high/too low)")
