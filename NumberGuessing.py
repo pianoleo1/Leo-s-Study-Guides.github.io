9f31411432f2213c0e6c35202a3ca256941518e4
pianoleo1 <leozhang0331@gmail.com>
2025-02-04 09:42:16 -0800

    Update NumberGuessing.py

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -2,13 +2,13 @@
 userinput = int(input("Welcome to the number guessing game! Would you like to play computer guessing mode (1) or human mode (2)?"))
 if userinput == 1:
   print("Entering computer guessing mode...")
-  guessnum = input("What is the maximum number that I can guess? ")
+  guessnum = int(input("What is the maximum number that I can guess? "))
   while True:
     guessnum = guessnum//2
     userinput = input("Is it",guessnum,"? (too high/too low)")
     if userinput == "too high":
       guessnum = guessnum//2
       print("Is it",guessnum,"? (too high/too low)")
-    If userinput == "too low":
+    if userinput == "too low":
       guessnum = guessnum + guessnum//2
       print("Is it",guessnum,"? (too high/too low)")
