d01e302268cc7f2c46099acca15e9cfc1777c354
pianoleo1 <leozhang0331@gmail.com>
2025-02-04 18:52:36 -0800

    update

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -4,17 +4,40 @@ if userinput == 1:
   print("Entering computer guessing mode...")
   guessnum = int(input("What is the maximum number that I can guess? "))
   guessnum = guessnum//2
+  prevnum = guessnum
   userinput = input(f"Is it {guessnum}? (too high/too low) ")
-  while True:
-    if userinput == "high":
-      guessnum = guessnum - guessnum//2
-      userinput = input(f"Is it {guessnum}? (too high/too low/yes) ")
-      if userinput == "yes":
-        print("Yay! I guessed correctly. The number is ",guessnum,".")
-        quit()
-    if userinput == "low":
-      guessnum = guessnum + (guessnum*2 - guessnum)//2
-      userinput = input(f"Is it {guessnum}? (too high/too low/yes) ")
-      if userinput == "yes":
-        print("Yay! I guessed correctly. The number is ",guessnum,".")
-        quit()
+  if userinput == "high":
+    userinput = input("Is it 250? ")
+    while True:
+      if userinput == "high":
+        guessnum = guessnum - (guessnum - prevnum)//2
+        userinput = input(f"Is it {guessnum}? (too high/too low/yes) ")
+        prevnum = guessnum
+        if userinput == "yes":
+          print("Yay! I guessed correctly. The number is",guessnum,".")
+          quit()
+      if userinput == "low":
+        guessnum = guessnum + (guessnum*2 - guessnum)//2
+        userinput = input(f"Is it {guessnum}? (too high/too low/yes) ")
+        prevnum = guessnum
+        if userinput == "yes":
+          print("Yay! I guessed correctly. The number is",guessnum,".")
+          quit()
+
+  if userinput == "low":
+    userinput = input("Is it 750? ")
+    while True:
+      if userinput == "high":
+        guessnum = guessnum - (guessnum - prevnum)//2
+        userinput = input(f"Is it {guessnum}? (too high/too low/yes) ")
+        prevnum = guessnum
+        if userinput == "yes":
+          print("Yay! I guessed correctly. The number is",guessnum,".")
+          quit()
+      if userinput == "low":
+        guessnum = guessnum + (guessnum*2 - guessnum)//2
+        userinput = input(f"Is it {guessnum}? (too high/too low/yes) ")
+        prevnum = guessnum
+        if userinput == "yes":
+          print("Yay! I guessed correctly. The number is",guessnum,".")
+          quit()
